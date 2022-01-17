aa3bb2865d9d15931552109fc1c325cc2232507f
jungmln0078 <jungmln0078@bluedigm.com>
2022-01-17 18:56:23 +0900

    Logging

--- src/main/java/example/mybatis/blog/interceptor/CustomInterceptor.java
+++ src/main/java/example/mybatis/blog/interceptor/CustomInterceptor.java
@@ -3,6 +3,7 @@ package example.mybatis.blog.interceptor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -13,8 +14,14 @@ public class CustomInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        LOGGER.info("===================    START    ===================");
         LOGGER.info(request.getMethod() + " " + request.getRequestURI());
         LOGGER.info(handler.toString());
         return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        LOGGER.info("====================    END    ====================");
     }
 }
